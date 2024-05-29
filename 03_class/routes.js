e5da7ab6a7332a0a88d5de4355ad69b46663bc7d
diegomadariaga <diegonicolasmadariaga@gmail.com>
2024-05-29 14:50:51 -0400

    refactor: Update routes.js to include movie endpoints

--- 03_class/routes.js
+++ 03_class/routes.js
@@ -1,24 +1,39 @@
 const express = require('express');
 const app = express();
+const movies = require('./movies.json');
+
 app.disable('x-powered-by');
 app.use(express.json());
 
 app.use((_req, _res, next) => {
     console.log('middleware 1');
-    next();
+    return next();
 });
 
 app.get('/', (_req, res) => {
-    res.send('Hello World!');
+    return res.send('Hello World!');
 });
 
 app.post('/', (req, res) => {
     const response = { message: 'Data received', data: req.body };
-    res.json(response);
+    return res.json(response);
+});
+
+//movies
+app.get('/movies', (_req, res) => {
+    return res.json(movies);
+});
+app.get('/movies/:id', (req, res) => {
+    const id = req.params.id;
+    const movie = movies.find((movie) => movie.id === id);
+    if (!movie) {
+        return res.status(404).send({ message: 'Movie not found' });
+    }
+    return res.json(movie);
 });
 
 app.use((_req, res) => {
-    res.status(404).send('Not Found');
+    return res.status(404).send('Not Found');
 });
 
 module.exports = app;
