25ed2a88233eae8b7e17e290f8da7db0b52ceb76
diegomadariaga <diegonicolasmadariaga@gmail.com>
2024-05-29 00:20:51 -0400

    chore: Add JSON body parsing and logging middleware to Express app

--- 03_express.js
+++ 03_express.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const app = express();
 app.disable('x-powered-by');
+app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log('your json body:', req.body);
+    next();
+});
 
 app.get('/', (_req, res) => {
     res.send('Hello World!');
+});
+
+app.post('/', (req, res) => {
+    const response = { message: 'Data received', data: req.body };
+    res.json(response);
+});
+
+app.use((_req, res) => {
+    res.status(404).send('Not Found');
 });
 
 module.exports = app;
--- 03_express.test.js
+++ 03_express.test.js
@@ -1,9 +1,16 @@
 const supertest = require('supertest');
 const app = require('./03_express.js');
 
-describe('express app', () => {   
+describe('express app', () => {
     it('responds with Hello World!', async () => {
         const response = await supertest(app).get('/');
         expect(response.text).toBe('Hello World!');
+    });
+    describe('POST /', () => {
+        it('responds with JSON', async () => {
+            const body = { key: 'value' };
+            const response = await supertest(app).post('/').send(body);
+            expect(response.body).toEqual({ message: 'Data received', data: body });
+        });
     });
 });
