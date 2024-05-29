2bb56c992e5313916036efa86fcc578f8b923d3c
diegomadariaga <diegonicolasmadariaga@gmail.com>
2024-05-29 14:32:12 -0400

    feat: Add movies.json file with movie data

--- 03_class/routes.js
+++ 03_class/routes.js
@@ -3,8 +3,8 @@ const app = express();
 app.disable('x-powered-by');
 app.use(express.json());
 
-app.use((req, res, next) => {
-    console.log('your json body:', req.body);
+app.use((_req, _res, next) => {
+    console.log('middleware 1');
     next();
 });
 
