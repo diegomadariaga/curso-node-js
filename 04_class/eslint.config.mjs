e1734a930cd5043f1c899ea8b44d35354a5cc23c
diegomadariaga <diegonicolasmadariaga@gmail.com>
2024-05-30 13:03:20 -0400

    refactor: Update ESLint configuration to use ES modules and include jest global variable

--- 04_class/eslint.config.mjs
+++
@@ -1,9 +0,0 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
-
-export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
-  {languageOptions: { globals: globals.node, jest: true }},
-  pluginJs.configs.recommended,
-];
