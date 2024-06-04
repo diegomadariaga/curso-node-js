9eb970ec53c6812ad82d94c49818806f3f374ad5
diegomadariaga <diegonicolasmadariaga@gmail.com>
2024-06-04 00:52:19 -0400

    feat: add connection feature

--- 06_class_chat/server/index.js
+++ 06_class_chat/server/index.js
@@ -1,17 +1,27 @@
 import express from 'express';
 import logger from 'morgan';
-
+import { Server } from 'socket.io';
+import { createServer } from 'http';
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
 
 app.use(logger('dev'));
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
 
 app.get('/', (req, res) => {
     res.sendFile(process.cwd() + '/client/index.html');
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
