203f29dbf73137ea68c54bd802802100007a191c
diegomadariaga <diegonicolasmadariaga@gmail.com>
2024-06-17 17:01:43 -0400

    refactor: code to use Express framework

--- 07_user_auth/index.js
+++ 07_user_auth/index.js
@@ -1,1 +1,13 @@
-console.log('hola mundo!')
+import express from 'express'
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+
+app.get('/', (req, res) => {
+  return res.send('Hello World')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
